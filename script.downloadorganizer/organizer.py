a11d1a50f6a45b19a9a6832e2555c2f0b7ad0da8
Alexandre Vázquez <alexandre.vazquez@gmail.com>
2015-10-24 15:46:33 +0200

    Several fixes to avoid errors during the organization of the video files

--- script.downloadorganizer/organizer.py
+++ script.downloadorganizer/organizer.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import xbmc
+import shutil
 import organizer
 
 def generate_final_path(tmp_file,tvshowsRootFolder,moviesRootFolder):
@@ -13,7 +14,7 @@ def generate_final_path(tmp_file,tvshowsRootFolder,moviesRootFolder):
             tvshows = groups[1];
             if tvshows != "":
                   tvshows = tvshows.replace("."," ")
-                  tvshows = tvshows.title()            
+                  tvshows = tvshows.title()
             xbmc.log("TV Show: "+tvshows)
             season = groups[2]
             if season != "":
@@ -31,19 +32,37 @@ def generate_final_path(tmp_file,tvshowsRootFolder,moviesRootFolder):
             dirPath = moviesRootFolder + "/" + os.path.splitext(base_file)[0]
             xbmc.log("Dir path: "+dirPath)
             finalPath =  dirPath + "/" + base_file
-      return finalPath     
+      return finalPath
 
 
 def process_file(tmp_file,tvshowsRootFolder,moviesRootFolder):
       try:
               finalPath = generate_final_path(tmp_file,tvshowsRootFolder,moviesRootFolder)
               try:
+                    xbmc.log("Starting to move the file to: "+finalPath)
                     folder = os.path.dirname(finalPath)
                     if folder != "":
-                        os.makedirs(folder);
-                        os.rename(tmp_file,finalPath);
+                        if not os.path.isdir(folder):
+                            xbmc.log("Creating the folder if they don't exist.")
+                            os.makedirs(folder);
+                            xbmc.log("Created the folder if they don't exist.")
+                        if not os.path.isfile(finalPath):
+                              try:
+                                  xbmc.log("Renaming the file")
+                                  os.rename(tmp_file,finalPath)
+                                  xbmc.log("Renamed the file")
+                              except Exception as e:
+                                    xbmc.log("Error renaming the file: "+str(e))
+                                    xbmc.log("Trying to copy the file")
+                                    shutil.copy(tmp_file,finalPath)
+                                    xbmc.log("File copied")
+                        else:
+                            xbmc.log("Removing the original file")
+                            os.remove(tmp_file)
+                            xbmc.log("Removed the original file")
+                    xbmc.log("Ending to move the file to: "+finalPath)
               except Exception as e:
-                  xbmc.log("Error processing the file: "+str(e))                      
+                  xbmc.log("Error processing the file: "+str(e))
       except Exception as e:
             xbmc.log("Error processing the file: "+str(e))
 
@@ -63,5 +82,5 @@ def organize(downloadFolder,tvshowsRootFolder,moviesRootFolder,extensions):
 
    xbmc.log("Filtered recovered files: "+str(len(filtered_files)))
 
-   for tmp_file in filtered_files:        
+   for tmp_file in filtered_files:
         process_file(tmp_file,tvshowsRootFolder,moviesRootFolder)
